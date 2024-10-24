6e9a59a814c620b13a6e0c31422d22b6752c0bd3
santi142004 <santiagobarreralopez2004@gmail.com>
2024-10-24 15:50:23 -0500

    cambios port

--- src/main/java/com/LatinasSexCam/LatinasSexCamApplication.java
+++ src/main/java/com/LatinasSexCam/LatinasSexCamApplication.java
@@ -3,11 +3,19 @@ package com.LatinasSexCam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class LatinasSexCamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LatinasSexCamApplication.class, args);
+		SpringApplication app = new SpringApplication(LatinasSexCamApplication.class);
+		String port = System.getenv("PORT");
+		if (port != null){
+			app.setDefaultProperties(Collections.singletonMap("server.port",port));
+		}
+		app.run(args);
+/*		SpringApplication.run(LatinasSexCamApplication.class, args);*/
 	}
 
 }
